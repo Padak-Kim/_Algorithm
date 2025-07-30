06a1f4eadfe6edca85e570e79f58571cf7c1c28a
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-07-30 16:59:13 +0900

    [level 3] Title: 정수 삼각형, Time: 1.14 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -7,12 +7,19 @@ using namespace std;
 int solution(vector<vector<int>> triangle) {
     int N = triangle.size();
 
-    // triangle을 그대로 dp로 활용
-    for (int y = N - 2; y >= 0; --y) {
+    // dp[y][x]: triangle[y][x]까지 내려왔을 때 최대 합
+    vector<vector<int>> dp(N, vector<int>(N, 0));
+    dp[0][0] = triangle[0][0];  // 시작점 초기화
+
+    for (int y = 1; y < N; ++y) {
         for (int x = 0; x <= y; ++x) {
-            triangle[y][x] += max(triangle[y + 1][x], triangle[y + 1][x + 1]);
+            if (x > 0)
+                dp[y][x] = max(dp[y][x], dp[y - 1][x - 1] + triangle[y][x]);
+            if (x < y)
+                dp[y][x] = max(dp[y][x], dp[y - 1][x] + triangle[y][x]);
         }
     }
 
-    return triangle[0][0];  // 최종 최대 경로 합
+    // 마지막 줄에서 최대값 반환
+    return *max_element(dp[N - 1].begin(), dp[N - 1].end());
 }
