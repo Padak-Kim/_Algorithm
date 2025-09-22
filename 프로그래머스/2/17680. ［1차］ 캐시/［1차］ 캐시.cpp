54b839b05f924b5a0a9893148444e3579c371c80
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-09-22 18:11:34 +0900

    [level 2] Title: [1차] 캐시, Time: 25.21 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,40 +1,45 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <list>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
+    
     list<string> li;
-    unordered_map<string, list<string>::iterator> um;
+	unordered_map<string, list<string>::iterator> um;
     
     if (cacheSize == 0) return cities.size() * 5;
-    
-    for(int i = 0; i < cities.size(); i++)
+    for(int i = 0 ; i < cities.size(); i++)
     {
-        string key = cities[i];
-        transform(key.begin(), key.end(), key.begin(), ::tolower);
-        if (um.find(key) == um.end())   // 새 원소
+        string city = cities[i];
+        transform(city.begin(), city.end(), city.begin(), ::tolower);
+        auto it = um.find(city);
+        if (it != um.end()) // 캐시에 있음
         {
-            if (li.size() == cacheSize)
+            // 삭제 후 뒤로 보내기
+            li.erase(it->second);
+            um.erase(city);
+            answer += 1;
+        }
+        else
+        {
+            // 캐시에 없음
+            // 꽉 찼으면 가장 오래된거 제거
+            if (li.size() >= cacheSize)
             {
-                // 꽉 차서 가장 마지막에 추가된 것 삭제
-                string last = li.back(); li.pop_back();
-                um.erase(last);
+                um.erase(*li.begin());
+                li.pop_front();   
             }
-            answer += 5;
+            answer += 5;    
         }
-        else // 이미 있음
-        {
-            li.erase(um[key]);
-            answer++;
-        }
-        li.push_front(key);
-        um[key] = li.begin();
+        
+        // 새로 삽입
+        li.push_back(city);
+        um[city] = prev(li.end());
     }
-    
     return answer;
 }
