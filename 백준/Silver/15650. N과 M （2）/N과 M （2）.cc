322dec7d1f480d476b1eaecee9b790049d5e4bea
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-10-16 16:39:56 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <climits>
+#include <sstream>
+using namespace std;
+
+void DFS(int n, int m, vector<int>& visited, vector<int>& arr, int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (visited[i] == 0)
+		{
+			if (cnt > 0 && arr[cnt - 1] > i)	continue;
+			arr[cnt] = i;
+			visited[i] = 1;
+			DFS(n, m, visited, arr, cnt + 1);
+			visited[i] = 0;
+		}
+	}
+}
+int main() 
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> visited(n + 1, 0);
+	vector<int> arr(n + 1, 0);
+	
+	DFS(n, m, visited, arr, 0);
+
+}
