4cd2fc11ecd457d54f0db4493ba70d24f2341cab
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-10-21 18:22:33 +0900

    Update AC.cc

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,98 +1,89 @@
-#include <iostream>
-#include <algorithm>
 #include <string>
-#include <deque>
-
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <climits>
+#include <sstream>
+#include <math.h>
 using namespace std;
-deque<int> dq;
 
-int main()
+int main() 
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; t++)
+	{
+		string cmd; cin >> cmd;
+		int n; cin >> n;
 
-    int n;
-    cin >> n;
+		string a; cin >> a;
+		vector<int> v;
+		v.reserve(n);
+		
+		string num = "";
+		for (int i = 0; i < a.size(); i++)
+		{
+			if (a[i] >= '0' && a[i] <= '9')
+				num += a[i];
+			else
+			{
+				if (num != "")
+				{
+					v.push_back(stoi(num));
+					num = "";
+				}
+			}
+		}
+		
+		vector<int> res(v.size());
 
-    for (int i = 0; i < n; i++)
-    {
-        string func;
-        cin >> func;
-        int n;
-        cin >> n;
-        string arr;
-        cin >> arr;
+		bool error = false;
+		bool R = false;
+		for (char c : cmd)
+		{
+			if (c == 'R')
+				R = !R;
+			else
+			{
+				if (v.empty())
+				{
+					cout << "error\n";
+					error = true;
+					break;
+				}
+				else
+				{
+					if (R)
+						v.erase(v.end() - 1);
+					else
+						v.erase(v.begin());
+				}
+			}
+		}
+		if (!error)
+		{
+			if (R)
+				reverse(v.begin(), v.end());
 
-        string num = "";
+			cout << "[";
+			for (int i = 0; i < v.size(); i++)
+			{
+				cout << v[i];
+				if (i != v.size() - 1)
+					cout << ",";
+			}
 
-        for (int i = 0; i < arr.length(); i++)
-        {
-            if (arr[i] == '[')
-                continue;
-            
-            if (arr[i] >= '0' && arr[i] <= '9')
-            {
-                num.push_back(arr[i]);
-            }
-            if (arr[i] == ',' || arr[i] == ']')
-            {
-                if (num != "") 
-                {
-                    dq.push_back(stoi(num));
-                    num = "";
-                }
-            }
-        }
-
-        bool reverse = false;
-        bool error = false;
-        for (int i = 0; i < func.length(); i++)
-        {
-            if (func[i] == 'D')
-            {
-                if (dq.empty())
-                {
-                    cout << "error" << "\n";
-                    error = true;
-                    break;
-                }
-                if (reverse)
-                    dq.pop_back();
-                else
-                    dq.pop_front();
-            }
-            else
-            {
-                reverse = !reverse;
-            }
-        }
-        
-        if (error)
-            continue;
-
-        cout << "[";
-        while (!dq.empty())
-        {
-            if (reverse)
-            {
-                cout << dq.back();
-                dq.pop_back();
-            }
-            else
-            {
-                cout << dq.front();
-                dq.pop_front();
-            }         
-            if (dq.empty())
-                break;
-            else
-                cout << ",";
-        }
-        cout << "]\n";
-
-        reverse = false;
-    }
-
-    return 0;
+			cout << "]\n";
+		}
+	}
 }
