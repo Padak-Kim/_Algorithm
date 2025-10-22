8993c668a8fb0151dfcc86355c6ebd512d473c27
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-10-22 14:19:09 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.88 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,33 +1,51 @@
 #include <string>
-#include <stack>
 #include <vector>
+#include <stack>
+#include <iostream>
 using namespace std;
 
-bool isValid(string s) {
-    stack<char> st;
-    for (char c : s) {
-        if (c == '(' || c == '{' || c == '[') {
-            st.push(c);
-        } else {
-            if (st.empty()) return false;
-            char top = st.top();
-            st.pop();
-            if ((c == ')' && top != '(') ||
-                (c == '}' && top != '{') ||
-                (c == ']' && top != '[')) {
+bool IsValid(string str)
+{
+    stack<char> s;
+    for(char& c : str)
+    {
+        if (c == '[' || c == '(' || c == '{')
+            s.push(c);
+        else
+        {
+            if (s.empty())
                 return false;
+            else
+            {
+                char t = s.top();
+                
+                if ((c == ']' && t == '[') || (c == ')' && t == '(') || (c == '}' && t == '{'))
+                    s.pop();
+                else
+                    return false;
             }
         }
     }
-    return st.empty();
+    if (s.empty())
+        return true;
+    else
+        return false;
 }
 
 int solution(string s) {
-    int n = s.size();
     int answer = 0;
-    for (int i = 0; i < n; i++) {
-        string rotated = s.substr(i) + s.substr(0, i); // i칸 회전
-        if (isValid(rotated)) answer++;
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if (IsValid(s))
+            answer++;
+        string temp = "";
+        char c = s.front();
+        temp = s.substr(1);
+        temp += c;
+        
+        s = temp;
     }
+    
     return answer;
 }
