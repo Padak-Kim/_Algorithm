d28c0709576c092ffbc5e871170e4c5d0a9ea395
Padak <79506107+Padak-Kim@users.noreply.github.com>
2025-10-23 17:02:59 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,41 +1,45 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds) {
+int CalculationDate(int progress, int speed)
+{
+    int day = (100 - progress) / speed;
+    if ((100 - progress) % speed != 0) // 나머지가 나오면 +1
+        day++;
+    
+    return day;
+}
+vector<int> solution(vector<int> progresses, vector<int> speeds) 
+{
     vector<int> answer;
     
-    for (int i = 0; i < progresses.size(); i++)
+    int proceeding = 0;
+    int cnt = 0;
+    for(int i = 0; i < progresses.size(); i++)
     {
-        int count = 1; // 배포 개수
-        
-        
-        int day = (100 - progresses[i]) / speeds[i]; // 배포 일 수
-        
-        if ((100 - progresses[i]) % speeds[i] != 0) // 작업이 남았으면 +1
-            day++;
-        
-        while(i + 1 < progresses.size())
+        int day = CalculationDate(progresses[i], speeds[i]);
+        if (proceeding == 0)
         {
-            int nextDay = (100 - progresses[i + 1]) / speeds[i + 1];
-            
-            if ((100 - progresses[i + 1]) % speeds[i + 1] != 0) // 작업이 남았으면 +1
-                nextDay++;
-            
-            if (day >= nextDay)
+            proceeding = day;
+            cnt++;
+        }
+        else
+        {
+            if (proceeding >= day)
             {
-                count++;
-                i++;
+                cnt++;
             }
             else
             {
-                break;
+                answer.push_back(cnt);
+                proceeding = day;
+                cnt = 1;
             }
         }
-        
-        answer.push_back(count);
     }
     
+    answer.push_back(cnt);
     return answer;
 }
