04e9f8ce7c042222331e31b035c4dbc83dba8739
Padak <79506107+Padak-Kim@users.noreply.github.com>
2026-01-23 15:02:13 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 4456 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -1,92 +1,77 @@
-#include <string>
-#include <vector>
-#include <stack>
 #include <iostream>
-#include <queue>
-#include <set>
-#include <list>
-#include <algorithm>
+#include <string>
+#include <sstream>
+#include <deque>
+#include <vector>
 #include <unordered_map>
 #include <unordered_set>
 #include <queue>
+#include <climits>
+#include <cmath>
+#include <algorithm>
 using namespace std;
+vector<int> parent;
 
-struct DSU {
-	vector<int> parent, rank;
+struct Edge
+{
+    int u; 
+    int v;
+    int w;
 
-	DSU(int n) : parent(n), rank(n, 1) {
-		for (int i = 0; i < n; i++){
-			parent[i] = i;
-		}
-	}
-
-	int find(int u) {
-		if (u == parent[u]) return u;
-		return parent[u] = find(parent[u]);
-	}
-
-	void merge(int a, int b)
-	{
-		a = find(a);
-		b = find(b);
-
-		if (a == b)
-			return;
-
-		if (rank[a] < rank[b])
-			swap(a, b);
-		parent[b] = a;
-		if (rank[a] == rank[b])
-			rank[a]++;
-
-		return;
-	}
-
-	bool same(int a, int b) {
-		return find(a) == find(b);
-	}
+    bool operator<(const Edge& e) const {
+        return w < e.w;
+    }
 };
-struct Edge {
-	int u;
-	int v;
-	int cost;
+int find(int x)
+{
+    if (parent[x] == x) 
+        return x;
 
-	bool operator<(const Edge& e) const {
-		return cost < e.cost;
-	}
-};
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	int n; // 컴퓨터의 수
-	cin >> n;
-	int m; // 연결할 수 있는 선의 수
-	cin >> m;
+    return parent[x] = find(parent[x]);
+}
 
-	vector<vector<int>> graph(n + 1, vector<int>(n + 1, -1));
-	vector<Edge> edges;
-	for (int i = 0; i < m; i++){
-		int a, b, c;	// a컴퓨터와 b컴퓨터를 연결하는데 비용 c (a == b 일수도 있음)
-		cin >> a >> b >> c;
-		graph[a][b] = c;
+void unite(int a, int b)
+{
+    a = find(a);
+    b = find(b);
 
-		edges.push_back({ a, b, c });
-	}
-	// 비용순으로 정렬
-	sort(edges.begin(), edges.end());
-	
-	DSU dsu(n + 1);
+    if (a == b) return;
 
-	int totalCost = 0;
-	for (const auto& edge : edges) {
-		if (!dsu.same(edge.u, edge.v))
-		{
-			dsu.merge(edge.u, edge.v);
-			totalCost += edge.cost;
-		}
-	}
+    parent[b] = a;
+}
 
-	cout << totalCost << '\n';
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<Edge> edges;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edges.push_back({ a, b, c });
+    }
+
+    sort(edges.begin(), edges.end());
+
+    parent.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+        parent[i] = i;
+
+    int totalCost = 0;
+    for (auto& e : edges)
+    {
+        if (find(e.u) != find(e.v))
+        {
+            unite(e.u, e.v);
+            totalCost += e.w;
+        }
+    }
+
+    cout << totalCost << "\n";
+    return 0;
 }
