ddd5f1d2f9d6fb161008e8c702d88580b0312da4
Padak <79506107+Padak-Kim@users.noreply.github.com>
2026-02-16 09:59:56 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.85 ms, Memory: 6.99 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int solution(vector<int> sticker)
 {
-    int n = sticker.size();
-    if (n == 1) return sticker[0];
-    
-    // 0번 뽑았을 때
-    vector<int> dp1(n, 0);
-    dp1[0] = sticker[0];
-    dp1[1] = max(sticker[0], sticker[1]);
-    
-    for(int i = 2; i < n - 1; i++)
-    {
-        dp1[i] = max(dp1[i - 1], dp1[i - 2] + sticker[i]);
-    }
-    
-    vector<int> dp2(n, 0);
-    dp2[1] = sticker[1];
-    
-    for (int i = 2; i < n; i++)
-    {
-        dp2[i] = max(dp2[i - 1], dp2[i - 2] + sticker[i]);
-    }
-    
-    return max(dp1[n - 2], dp2[n - 1]);
+    int answer =0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
 }
