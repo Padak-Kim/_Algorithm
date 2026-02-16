61ec074c054921ea8165f3d6061f75691117f34f
Padak <79506107+Padak-Kim@users.noreply.github.com>
2026-02-16 09:34:56 +0900

    [level 2] Title: 땅따먹기, Time: 12.92 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int> > land)
 {
-    int m = 4;
-    int n = land.size();
-    
-    vector<vector<int>> dp(n, vector<int>(m, 0));
-    dp[0][0] = land[0][0];
-    dp[0][1] = land[0][1];
-    dp[0][2] = land[0][2];
-    dp[0][3] = land[0][3];
-    for(int i = 1; i < n; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            int bestPrev = 0;
-            for(int p = 0; p < 4; p++)
-            {
-                if (p == j) 
-                    continue;
-                bestPrev = max(bestPrev, dp[i - 1][p]);
-            }
-            
-            dp[i][j] = bestPrev + land[i][j];
-        }
-    }
+    int answer = 0;
 
-    return max({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2], dp[n - 1][3]});
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
 }
